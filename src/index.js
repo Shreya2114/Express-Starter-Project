80ac903eb04deb823ddddad523e582d34011b25b
Shreya Rai <raishreya1421@gmail.com>
2024-11-29 19:17:40 +0530

    connected to mongoDB server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const serverConfig = require('./config/serverConfig')
+const connectDB = require('./config/dbConfig')
 
 const PORT = process.env.PORT
 
 const app = express()
-app.listen(PORT, () =>{
-    console.log(`Server started at port ${serverConfig.PORT}`)
-   
+app.listen(serverConfig.PORT, async () =>{
+    await connectDB();
+    console.log(`Server started at port ${serverConfig.PORT}`);
+    
 }) 
+
+
+
+// x7lm3lBn91fXn1CC
+// raishreya1421
